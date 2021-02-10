20256e72787f08616a197b28d3b61af6000a73e8
Joao Duarte <joaopd17@hotmail.com>
2021-02-10 16:02:19 +0100

    Update css styling to card.css

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ export default function App() {
     .catch(error => errorHandler(error))
 
   function createCards(characters) {
-    const cards = characters.map(character => Card(character.name))
+    const cards = characters.map(character =>
+      Card(character.image, character.name, character.house)
+    )
     document.body.append(...cards)
   }
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,29 @@
 import './Card.css'
 import createElement from '../../lib/createElement'
 
-export default function Card(name) {
-  return createElement('section', { className: 'Card' }, name)
+export default function Card(imagePath, name, house) {
+  const characterImage = createElement('img', { src: imagePath })
+  const characterName = createElement('h2', { innerText: name })
+  const characterHouse = createElement('p', { innerText: house })
+
+  const el = createElement(
+    'section',
+    { className: 'Card' },
+    characterName,
+    characterHouse,
+    characterImage
+  )
+  return el
 }
+
+// const houseColors = function identifyHouseColor() {
+//   if (house === 'Gryffindor') {
+//     return (Card.style.background = '#c50000')
+//   } else if (house === 'Slytherin') {
+//     return (Card.style.background = '#2a623d')
+//   } else if (house === 'Hufflepuff') {
+//     return (Card.style.background = '#ffdb00')
+//   } else {
+//     return (Card.style.background = '#1d72b4')
+//   }
+// }
